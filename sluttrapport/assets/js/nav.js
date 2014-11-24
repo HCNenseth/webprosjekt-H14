b85e9dab0d8bf1b1c21beb7e93346edcb57816d1
Alexander Skjolden <alex@plastboks.net>
2014-11-24 09:20:10 +0100

    nav fix, sluttrapport
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- sluttrapport/assets/js/nav.js
+++ sluttrapport/assets/js/nav.js
@@ -2,15 +2,8 @@ function toggle_visibility(id) {
   var antall = document.getElementsByClassName('nav-ul');
   var e = document.getElementById(id);
 
-    if ( e.style.display != 'none' )
-      {
-        e.setAttribute('style', 'display: none');
-      }
-    else {
-      for ( var i = 0; i < antall.length; i++ )
-        {
-          antall[i].setAttribute('style', 'display: none');
-        }
-        e.setAttribute('style', 'display: block');
-    }
+  for ( var i = 0; i < antall.length; i++ ) {
+      antall[i].setAttribute('style', 'display: none');
   }
+  e.setAttribute('style', 'display: block');
+}
