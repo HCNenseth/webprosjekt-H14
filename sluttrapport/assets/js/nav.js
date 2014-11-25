fdf5c9326703a88f22c5d9596c01b69f2a8d0ef7
Hans Petter Osvold <osvold89@gmail.com>
2014-11-25 19:55:46 +0100

    innhold

--- sluttrapport/assets/js/nav.js
+++ sluttrapport/assets/js/nav.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+  responsiveScrollBy();
+}
+
 function toggle_visibility(id) {
   var antall = document.getElementsByClassName('nav-ul');
   var e = document.getElementById(id);
@@ -6,4 +10,13 @@ function toggle_visibility(id) {
       antall[i].setAttribute('style', 'display: none');
   }
   e.setAttribute('style', 'display: block');
+}
+
+function responsiveScrollBy(){
+  var window_width = window.innerWidth;
+  if( ( window_width > 1 ) && ( window_width < 481 ) ){
+        var innerHeight = document.getElementById('naven').offsetHeight;
+        alert(innerHeight);
+        window.scrollBy(0, 1000);
+  }
 }
